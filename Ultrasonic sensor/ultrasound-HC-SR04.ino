4a6d337cd0dab49a2e28caa951fdb51fb5988845
nicolsc <nicolas.lesconnec@gmail.com>
2015-02-19 22:05:45 +0100

    Ultrasonic sensor distance measure HC-SR04 sensor

---
+++ Ultrasonic sensor/ultrasound-HC-SR04.ino
@@ -0,0 +1,40 @@
+#define trigPin 13
+#define echoPin 12
+
+
+enum {
+  TRIG_PIN = 13,
+  ECHO_PIN = 12,
+  
+  ECHO_DELAY = 2, //ms
+  
+  SERIAL_BAUD  = 9600,
+
+  POLL_DELAY   = 1000 //ms
+};
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(ECHO_DELAY);
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration/2) / 29.1;
+
+
+  if (distance >= 200 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(POLL_DELAY);
+}
